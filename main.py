68f2e8266be71f845d49b0ba28fab7da49f76493
Azim Sofi <azimmiskandar@gmail.com>
2023-09-02 21:54:13 +0800

    管理

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from Django_myanimelist import app
-from flask import render_template
+from flask import Flask, render_template
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app = Flask(__name__, template_folder='static/templates')
 
 @app.route('/')
 def index():
-
     name = 'JJSakurai'
-    return render_template(
-        'index.html',
-        name = name
-    )
+    return render_template('index.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
