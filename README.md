01c1ca1e71a20b5d852cef9864ad67394196852e
Azim Sofi <azimmiskandar@gmail.com>
2023-09-02 20:46:56 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Django_myanimelist
+ Cloning https://myanimelist.net/profile/ using Django, flask, jinja2
---
+++ main.py
@@ -0,0 +1,8 @@
+from Django_myanimelist import app
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template(
+        'index.html'
+    )
