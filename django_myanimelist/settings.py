f483063c6b43c7db83dd9f6cb48fd43548fcd6a4
rrumiyadayo <rrumiyadayo@yahoo.co.jp>
2023-09-05 23:41:23 +0900

    posgres kuso shine

--- django_myanimelist/settings.py
+++ django_myanimelist/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'django_myanimelist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_myanimelist',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
