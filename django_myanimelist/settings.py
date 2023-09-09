01404794e9bd0c2490c9c8c17170478e53464899
sShi1ro <muhammad.awparif.hakimi@gmail.com>
2023-09-09 14:55:04 +0800

    fontawesome bawah

--- django_myanimelist/settings.py
+++ django_myanimelist/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'fontawesomefree',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mal_userpage',
+    'fontawesomefree',
 ]
 
 MIDDLEWARE = [
